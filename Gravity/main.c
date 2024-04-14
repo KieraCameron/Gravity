fb993cd21b5aa590a1e986127875df2f56faa004
Kiera Cameron <j.yusufpro@gmail.com>
2024-04-14 17:40:29 +1200

    Initial Commit

---
+++ Gravity/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Gravity
+//
+//  Created by Yusuf Jaafar on 14/04/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
